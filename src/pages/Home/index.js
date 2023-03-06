57a504ac6c9425011aeb1d1ae0d34c859ad94d81
Noémie Houdou <houdou.noemie@gmail.com>
2023-03-06 10:27:54 +0100

    fix: manage footer thumbnail

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,11 @@ import Modal from '../../containers/Modal';
 import { useData } from '../../contexts/DataContext';
 
 const Page = () => {
-  const { last } = useData();
+  const { data } = useData();
+  const events = data?.events.sort(
+    (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)
+  );
+  const last = events[0];
   return (
     <>
       <header>
@@ -125,7 +129,7 @@ const Page = () => {
             title={last?.title}
             date={new Date(last?.date)}
             small
-            label="boom"
+            label={last?.type}
           />
         </div>
         <div className="col contact">
